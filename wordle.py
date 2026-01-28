d170aefc32fb0718523eabd4453de983134ad05e
Juan Gonzalez <juan.vicente.gonzalez93@gmail.com>
2026-01-28 14:50:23 -0300

    Se agrega un if para corroborar si gano o no el juego, para que funciones el if se crea una funcion convertir_secreto, que convierte la palabra secreta en otro formato de manera a corroborar si el usuario acerto o no

--- wordle.py
+++ wordle.py
@@ -1,6 +1,14 @@
 letras_verificadas = [] #se guardan las letras verificadas
 cantidad_letras = 5 #cantidad de letras que tiene la plabra 
 palabra_secreta = "calle" # palabra secreta 
+intentos = 0 #se pone en 0 la cantidad de intentos
+aux=[] #un auxiliar para guardar la palabra secreta formateada
+
+def convertir_secreto(): #se usa esta funcion para cambiar el formato de la palabra secreta, de manera hacer la comparacion final
+    for i in range(cantidad_letras):
+        aux.append(f"[{palabra_secreta[i]}]")
+
+convertir_secreto() 
 
 def verificador_palabra(palabra_ingresada, palabra_secreta):
     letras_verificadas = []
@@ -15,10 +23,14 @@ def verificador_palabra(palabra_ingresada, palabra_secreta):
             letras_verificadas.append(palabra_ingresada[i])#si no esta en la palabra
     return letras_verificadas
 
+
 while intentos < 6: #se encarga de que el juego termine despues de la cantidad de oportunidades definidas
     print(f"te quedan {6 - intentos} intentos")
     intentos = intentos + 1 #cada intento disminuye 1 
     palabra_ingresada = input("Ingrese una palabra: ")
     print(f"la palabra ingresada es: {palabra_ingresada}")
     print(verificador_palabra(palabra_ingresada, palabra_secreta)) #llama a la funcion que hace la corroboracion de letras en la palabra secreta
-  
+    letra_veri_final_juego = verificador_palabra(palabra_ingresada, palabra_secreta) # guarda la palabra para compararlo con la palabra secreta
+    if letra_veri_final_juego == aux: #hace la comparacion y si se True, termina el juego
+        print(f"Felicidades encontraste la palabra: {palabra_secreta}")
+        break
